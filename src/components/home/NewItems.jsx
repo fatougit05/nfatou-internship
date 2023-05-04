328bae7c2fa64ad9572a5a591d90a80a7af5245f
Fatou <117177588+fatougit05@users.noreply.github.com>
2023-05-04 18:02:17 +0000

    Update NewItems.jsx

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,93 +1,12 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
-import Countdown from "./Countdown";
-import { useNavigate } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css"
-import Skeleton from "../UI/Skeleton";
-const NewItems = ({item}) => {
-const [items,setItems] = useState([])
- const getItems = async () => {
-  const {data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
- setItems(data)
 
-}
-
-
-
-useEffect(() => {
-  getItems()
-  
-},[])
-
-useEffect(() =>{
-  AOS.init({duration:1600})
-},[])
-
-
-function Arrow(props) {
-  const { className, style, onClick } = props;
+const NewItems = () => {
   return (
-    <div
-      className={className}
-      style={{ ...style ,display: "block", background: "#CECECE", borderRadius:"60%", right: "1%",width:"30px", height:"30px", textAlign:"center" ,paddingTop:"6px"}}
-      onClick={onClick}
-    />
-  );
-}
-
-
-
-var settings = {
-  dots: true,
-  infinite: false,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  initialSlide: 0,
-  nextArrow: <Arrow />,
-  prevArrow: <Arrow />,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-        
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-};
-
-
-
-
-
-return (
     <section id="section-items" className="no-bottom">
-      <div className="container" data-aos="fade-up">
+      <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
@@ -95,31 +14,21 @@ return (
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-
-
-
-           { items.length > 0? 
-         ( <Slider {...settings} >
-       
-         { items.map((item, index) => (
-            <div className="" key={item.nftId} >
-              <div className="nft__item ml-3">
+          {new Array(4).fill(0).map((_, index) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+              <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
-                    to={`/author/${item.authorId}`}
+                    to="/author"
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
                     title="Creator: Monica Lucas"
                   >
-                    <img className="lazy" src={item.authorImage} alt="" />
+                    <img className="lazy" src={AuthorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-              {item?.expiryDate && <Countdown item={item} /> }
-    
-  
-
-
+                <div className="de_countdown">5h 30m 32s</div>
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -140,10 +49,9 @@ return (
                     </div>
                   </div>
 
-                  <Link to={`/item-details/${item.nftId}`}>
+                  <Link to="/item-details">
                     <img
-                      src={item.nftImage}
-                     
+                      src={nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -151,70 +59,21 @@ return (
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>{item.title}</h4>
+                    <h4>Pinky Ocean</h4>
                   </Link>
-                  <div className="nft__item_price">{item.price} ETH</div>
+                  <div className="nft__item_price">3.08 ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>{item.likes}</span>
+                    <span>69</span>
                   </div>
                 </div>
               </div>
             </div>
-))}
-</Slider>
-
-) :(
-
-  <>
-  <Slider {...settings} >
-               {  new Array(4).fill(0).map((_, index) => (
-                 <div className=""   >
-             
-                 <div className="nft_coll ml-3"   key={index}>
-                   <div className="nft_wrap " style={{height:"350px" }}>
-                   <div className="nft_coll_pp ml-5">
-                     <Link  to="/author">
-             <Skeleton width="50px" height="50px" backgroundColor="#ECECEC" borderRadius="50%"  marginTop="40px"/>
-                     </Link>
-                   
-                     <i className="fa fa-check"></i>
-                   </div>
-
-                     <Link to={``}>
-                      <Skeleton width="460px" height="730px" backgroundColor="#ECECEC" borderRadius={5}/>
-                     </Link>
-                   </div>
-                   
-                   <div className="nft_coll_info">
-          <div className="d-flex flex-column justify-content-center align-items-center">
- <Skeleton width="80px"  height="16px" backgroundColor="#ECECEC" marginTop="22px"/>
-                    
-                    <Skeleton width="100px"  height="12px" marginTop="10px" backgroundColor="#ECECEC"/>
+          ))}
         </div>
-               
-             
-               
-                   </div>
-               
-                 </div>
-               
-               </div>
-              
-                   ) )}
-               </Slider>
- </>
-
-)
-
-         }
-
-        </div>
-        </div>
-        
+      </div>
     </section>
   );
 };
 
 export default NewItems;
-
